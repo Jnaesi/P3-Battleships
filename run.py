78790f0a27a1c2b388c4116b70c94307a719e965
Jnaesi <jacobnasi@hotmail.com>
2022-07-18 00:52:41 +0000

    add rows to board

--- run.py
+++ run.py
@@ -3,7 +3,25 @@ HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 # Board for displaying hits and misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
-letters_to_numbers = { 'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,'H': 7}
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+letters_to_numbers = { 
+    'A': 0, 
+    'B': 1, 
+    'C': 2, 
+    'D': 3, 
+    'E': 4, 
+    'F': 5, 
+    'G': 6,
+    'H': 7
+    
+}
 
 def board():
     pass
@@ -18,3 +36,5 @@ def count_hit_ships():
     pass
 
 create_ships()
+turns = 10
+while turns > 0:
