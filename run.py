1b52a375bed7bdf784023882a4347d57bac80fa5
Jnaesi <jacobnasi@hotmail.com>
2022-07-19 19:24:56 +0000

    remove pprint, uploaded to heruko

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from random import randint
-from pprint import pprint
 
 #Board for holding ship locations
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
-# Board for displaying hits and misses
+#Board for displaying hits and misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
 def print_board(board):
